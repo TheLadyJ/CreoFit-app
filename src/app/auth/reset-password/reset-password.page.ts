22e23be39ca7b47d52d627ba7c98d2c69770036b
TheLadyJ <jovana.vida@gmail.com>
2024-04-12 22:31:40 +0200

    Added password reset page

--- src/app/auth/reset-password/reset-password.page.ts
+++ src/app/auth/reset-password/reset-password.page.ts
@@ -1,20 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {
+  IonContent,
+  IonCard,
+  IonItem,
+  IonInput,
+  IonText,
+  IonButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { keyOutline, mailOutline, personOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.page.html',
   styleUrls: ['./reset-password.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonIcon,
+    IonButton,
+    IonText,
+    IonInput,
+    IonItem,
+    IonCard,
+    IonContent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
 })
-export class ResetPasswordPage implements OnInit {
+export class ResetPasswordPage {
+  form!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ mailOutline, keyOutline, personOutline });
+    this.initForm();
   }
 
+  initForm() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    console.log(this.form.value);
+  }
 }
